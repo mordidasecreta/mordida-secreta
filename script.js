a1b0e1811459c0f03463faf387d42559e03c28d8
mordidasecreta <mordidasecreta.cali@gmail.com>
2026-06-28 00:10:35 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+/* ===========================
+   CONFIGURACIÓN
+=========================== */
 
+const callejeraDisponible = false;
+
+
+/* ===========================
+   CAMBIAR CANTIDAD
+=========================== */
+
+function cambiarCantidad(id, cambio) {
+
+  let elemento = document.getElementById(id);
+
+  let valor = parseInt(elemento.innerText);
+
+  valor += cambio;
+
+  if(valor < 0){
+    valor = 0;
+  }
+
+  elemento.innerText = valor;
+
+  actualizarTotal();
+}
